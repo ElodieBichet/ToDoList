0593e0255a212d3b4939c2ba17e693481b7ff8a6
Elodie Bichet <e_bichet@yahoo.fr>
2021-08-31 17:31:48 +0200

    Create first functional tests for security controller

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testDisplayLoginForm()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+        $this->assertSelectorNotExists('.alert.alert-danger');
+    }
+
+    public function testLoginWithBadCredentials()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' => 'fakeusername',
+            '_password' => 'fakepassword'
+        ]);
+        $client->submit($form);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+    }
+
+    public function testSuccessfullLogin()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' => 'Elodie',
+            '_password' => 'mdp-Elodie'
+        ]);
+        $client->submit($form);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Bienvenue');
+    }
+}
