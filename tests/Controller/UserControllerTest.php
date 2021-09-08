4cc0bc03f677756a822d140b083cf643170c3468
Elodie Bichet <e_bichet@yahoo.fr>
2021-09-08 18:01:04 +0200

    Update UserControllerTest to take new role into account

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use App\Tests\LoginUser;
+use App\DataFixtures\UserFixtures;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
@@ -22,24 +25,76 @@ class UserControllerTest extends WebTestCase
         $this->databaseTool = $this->testClient->getContainer()->get(DatabaseToolCollection::class)->get();
     }
 
-    /**
-     * @dataProvider usersRoutes
-     */
-    public function testUsersPageResponse($uri, $h1Text)
+    public function testUsersPageResponse()
     {
+        $this->testClient->request('GET', '/users/create');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'S\'enregistrer');
+    }
+
+    /**
+     * @dataProvider usersProtectedRoutes
+     */
+    public function testLoginRedirectionIfNotAuthenticated($uri): void
+    {
+        $this->testClient->request('GET', $uri);
+        $this->assertResponseRedirects(
+            "http://localhost/login",
+            Response::HTTP_FOUND,
+            "No redirection to login page for uri : " . $uri
+        );
+    }
+
+    public function usersProtectedRoutes()
+    {
+        return [
+            ['/users'],
+            ['/users/1/edit'],
+            ['/users/2/edit']
+        ];
+    }
+
+    /**
+     * @dataProvider usersAdminRoutes
+     */
+    public function testUsersPageResponseIfAuthenticatedAsAdmin($uri, $h1Text)
+    {
+        /** @var User */
+        $user = $this->databaseTool->loadFixtures([UserFixtures::class])->getReferenceRepository()->getReference('user-admin');
+
+        $this->login($this->testClient, $user);
+
         $this->testClient->request('GET', $uri);
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', $h1Text);
     }
 
-    public function usersRoutes()
+    public function usersAdminRoutes()
     {
         return [
-            ['/users', 'Liste des utilisateurs'],
             ['/users/create', 'Créer un utilisateur'],
+            ['/users', 'Liste des utilisateurs'],
             ['/users/1/edit', 'Modifier'],
             ['/users/2/edit', 'Modifier']
         ];
+    }
+
+    public function testForbiddenAccessForSimpleUser(): void
+    {
+        /** @var User */
+        $user = $this->databaseTool->loadFixtures([UserFixtures::class])->getReferenceRepository()->getReference('user-1');
+
+        $this->login($this->testClient, $user);
+
+        $this->testClient->request('GET', '/users');
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+        $this->testClient->request('GET', '/users/1/edit');
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+        $this->testClient->request('GET', '/users/3/edit');
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
     }
 }
